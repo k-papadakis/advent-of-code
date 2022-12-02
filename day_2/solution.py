3061cdbd81a81bee5595281723224d2d97ed34ff
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2022-12-02 13:31:39 +0200

    Completed day2

--- day_2/solution.py
+++ day_2/solution.py
@@ -1,26 +1,40 @@
 from itertools import starmap
-from typing import Literal
-from collections import namedtuple
 
-GameObject = namedtuple('GameObject', ['A', 'B', 'C', 'val'])
-GAME = {'X': GameObject(3, 0, 6, 1), 'Y': GameObject(6, 3, 0, 2), 'Z': GameObject(0, 6, 3, 3)}
+# Translating letters to numbers, so that we can use array indexing
+MAP = {'A': 0, 'B': 1, 'C': 2, 'X': 0, 'Y': 1, 'Z': 2}
+# MATCH[player][opponent] = points the player wins (excluding the value of his choice)
+MATCH = (3, 0, 6), (6, 3, 0), (0, 6, 3)
+# VAL[player] = the value of the player's choice
+VAL = 1, 2, 3
+# DECIDE[player][opponent] = The move the player must choose in part 2
+DECIDE = (2, 0, 1), (0, 1, 2), (1, 2, 0)
 
 
-def read_data(filename, sep):
-    with open(filename) as f:
+def read_data():
+    with open('input.txt') as f:
         for line in f:
-            opponent, player = line.rstrip('\n').split(sep)
-            yield opponent, player
+            opponent, player = line.rstrip('\n').split(' ')
+            yield MAP[player], MAP[opponent]
 
 
-def match_score(opponent: Literal['A', 'B', 'C'], player: Literal['X', 'Y', 'Z']):
-    g = GAME[player]
-    return getattr(g, opponent) + g.val
+def match_score1(player, opponent):
+    return MATCH[player][opponent] + VAL[player]
 
 
-def total_score(filename, sep=' '):
-    return sum(starmap(match_score, read_data(filename, sep)))
+def match_score2(player, opponent):
+    player = DECIDE[player][opponent]
+    return match_score1(player, opponent)
+
+
+def total_score(score_fn):
+    return sum(starmap(score_fn, read_data()))
+
+
+def main():
+    part1 = total_score(match_score1)
+    part2 = total_score(match_score2)
+    print(f'part1: {part1}\npart2: {part2}')
 
 
 if __name__ == '__main__':
-    print(total_score('input.txt', sep=' '))
+    main()
