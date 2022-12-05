13008ed1714835413169e7a3ea5a4789de1104ab
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2022-12-05 13:40:17 +0200

    completed day_05 part2

--- day_05/solution.py
+++ day_05/solution.py
@@ -1,3 +1,4 @@
+# %%
 from itertools import takewhile, dropwhile
 import re
 
@@ -11,10 +12,10 @@ def read_stacks():
                 if letter != ' ':
                     stacks[i].append(letter)
 
-        for stack in stacks:
-            stack.reverse()
+    for stack in stacks:
+        stack.reverse()
 
-        return stacks
+    return stacks
 
 
 def read_moves():
@@ -26,23 +27,46 @@ def read_moves():
             yield tuple(map(int, match.groups()))
 
 
-def do_move(stacks, instruction):
+def do_move_1(stacks, instruction):
     num, source, target = instruction
     for _ in range(num):
-        print(stacks)
         stacks[target-1].append(stacks[source-1].pop())
     return stacks
 
 
 # TODO: Implement a class
 
-def do_moves(stacks, instructions):
+def do_moves(stacks, instructions, move_fn):
     for instruction in instructions:
-        do_move(stacks, instruction)
+        move_fn(stacks, instruction)
     return stacks
 
+def part1():
+    stacks = read_stacks()
+    moves = read_moves()
+    stacks = do_moves(stacks, moves, do_move_1)
+    return ''.join(stack[-1] for stack in stacks)
+
+
+# %%
 
 stacks = read_stacks()
 moves = read_moves()
-stacks = do_moves(stacks, moves)
+
+def do_move_2(stacks, instruction):
+    num, source, target = instruction
+    t = []
+    for _ in range(num):
+        t.append(stacks[source-1].pop())
+    for _ in range(num):
+        stacks[target-1].append(t.pop())
+    return stacks
+
+
+do_moves(stacks, moves, do_move_2)
 ''.join(stack[-1] for stack in stacks)
+
+
+
+
+# %%
