b0e845917714e844f16a78e8d7b3f9e786a29d47
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2022-12-06 14:05:44 +0200

    Completed day_05

--- day_06/solution.py
+++ day_06/solution.py
@@ -1,33 +1,38 @@
-# %%
 from collections import deque
 
 
-def read_data():
-    with open('input.txt') as f:
+def read_data(input_file):
+    with open(input_file) as f:
         while (c := f.read(1)) != '\n':
             yield c
-            
 
-def part1(): 
-    stream = read_data()
+
+def first_n_unique(iterable, n):
     queue = deque()
     counter = {}
 
-    for i, right in enumerate(stream, 1):
-        
+    for i, right in enumerate(iterable, 1):
+
         queue.append(right)
         counter[right] = counter.get(right, 0) + 1
-        
-        if len(queue) < 4:
+
+        if len(queue) < n:
             continue
-        
-        if len(counter) == 4:
+
+        if len(counter) == n:
             return i
-        
+
         left = queue.popleft()
         counter[left] -= 1
         if counter[left] == 0:
             counter.pop(left)
-        
-    
-part1()
+
+
+def main():
+    part1 = first_n_unique(read_data('input.txt'), 4)
+    part2 = first_n_unique(read_data('input.txt'), 14)
+    print(f"part1: {part1}\npart2: {part2}")
+
+
+if __name__ == '__main__':
+    main()
