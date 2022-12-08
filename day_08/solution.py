472da61eeb324e014bb25e5c6aa02edda900a16e
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2022-12-08 14:42:09 +0200

    Part1 with clean generators

--- day_08/solution.py
+++ day_08/solution.py
@@ -1,70 +1,76 @@
-from itertools import accumulate
-from math import inf, prod
+import math
 
 
-def read_data(input_file) -> list[list[int]]:
+def read_data(input_file):
     with open(input_file) as f:
         return [list(map(int, line)) for line in f.read().splitlines()]
 
 
 def iter_rows(m, n):
     for i in range(m):
-        yield [(i, j) for j in range(n)]
+        yield ((i, j) for j in range(n))
 
 
 def iter_cols(m, n):
     for j in range(n):
-        yield [(i, j) for i in range(m)]
+        yield ((i, j) for i in range(m))
 
 
 def iter_rows_reversed(m, n):
     for i in range(m):
-        yield [(i, j) for j in reversed(range(n))]
+        yield ((i, j) for j in reversed(range(n)))
 
 
 def iter_cols_reversed(m, n):
     for j in range(n):
-        yield [(i, j) for i in reversed(range(m))]
+        yield ((i, j) for i in reversed(range(m)))
 
 
-def visibility(gen, a, v) -> None:
-    for indices in gen:
-        max_so_far = accumulate((a[i][j] for i, j in indices), max, initial=-inf)
-        for (i, j), M in zip(indices, max_so_far):
-            if a[i][j] > M:
-                v[i][j] = True
+def visibility(row_or_col_iter, array, visible):
+    for indices in row_or_col_iter:
+        max_so_far = -math.inf
+        for i, j in indices:
+            if array[i][j] > max_so_far:
+                max_so_far = array[i][j]
+                visible[i][j] = True
 
 
-def part1(a: list[list[int]]) -> int:
-    m, n = len(a), len(a[0])
-    v = [[False for _ in range(n)] for _ in range(m)]
+def part1(array):
+    m, n = len(array), len(array[0])
+    visible = [[False for _ in range(n)] for _ in range(m)]
 
     for gen in iter_rows(m, n), iter_cols(m, n), iter_rows_reversed(m, n), iter_cols_reversed(m, n):
-        visibility(gen, a, v)
+        visibility(gen, array, visible)
 
-    return sum(map(sum, v))
+    return sum(map(sum, visible))
 
 
-def find_visibility(a, i, j, direction):
+def find_visibility(array, i, j, direction):
 
-    m, n = len(a), len(a[0])
+    m, n = len(array), len(array[0])
     n_steps = 0
     x, y = i, j
 
     while (x := x + direction[0]) in range(m) and (y := y + direction[1]) in range(n):
         n_steps += 1
-        if a[x][y] >= a[i][j]:
+        if array[x][y] >= array[i][j]:
             break
 
     return n_steps
 
 
-def part2(a):
-    m, n = len(a), len(a[0])
+def part2(array):
+    m, n = len(array), len(array[0])
     directions = (0, 1), (1, 0), (-1, 0), (0, -1)
-    return max(
-        prod(find_visibility(a, i, j, direction) for direction in directions) for i in range(m) for j in range(n)
+
+    ret = max(
+        math.prod(find_visibility(array, i, j, direction)
+                  for direction in directions)
+        for i in range(m)
+        for j in range(n)
     )
+
+    return ret
 
 
 def main():
