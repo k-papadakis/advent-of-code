c046108209022ba49edbc05785b284c8252f0f61
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2022-12-09 10:29:26 +0200

    Working monotonic stack for 8.2 rows

--- day_08/solution.py
+++ day_08/solution.py
@@ -1,3 +1,5 @@
+# %%
+
 import math
 
 
@@ -45,38 +47,58 @@ def part1(array):
     return sum(map(sum, visible))
 
 
-def find_visibility(array, i, j, direction):
+# def find_visibility(array, i, j, direction):
 
-    m, n = len(array), len(array[0])
-    n_steps = 0
-    x, y = i, j
+#     m, n = len(array), len(array[0])
+#     n_steps = 0
+#     x, y = i, j
 
-    while (x := x + direction[0]) in range(m) and (y := y + direction[1]) in range(n):
-        n_steps += 1
-        if array[x][y] >= array[i][j]:
-            break
+#     while (x := x + direction[0]) in range(m) and (y := y + direction[1]) in range(n):
+#         n_steps += 1
+#         if array[x][y] >= array[i][j]:
+#             break
 
-    return n_steps
-
-
-def part2(array):
-    m, n = len(array), len(array[0])
-    directions = (0, 1), (1, 0), (-1, 0), (0, -1)
-
-    ret = max(
-        math.prod(find_visibility(array, i, j, direction)
-                  for direction in directions)
-        for i in range(m)
-        for j in range(n)
-    )
-
-    return ret
+#     return n_steps
 
 
-def main():
-    a = read_data('input.txt')
-    print(f'part1: {part1(a)}\npart2: {part2(a)}')
+# def part2(array):
+#     m, n = len(array), len(array[0])
+#     directions = (0, 1), (1, 0), (-1, 0), (0, -1)
+
+#     ret = max(
+#         math.prod(find_visibility(array, i, j, direction)
+#                   for direction in directions)
+#         for i in range(m)
+#         for j in range(n)
+#     )
+
+#     return ret
 
 
-if __name__ == '__main__':
-    main()
+# def main():
+#     a = read_data('input.txt')
+#     print(f'part1: {part1(a)}\npart2: {part2(a)}')
+
+
+# if __name__ == '__main__':
+#     main()
+
+
+# %%
+a = read_data('mini.txt')
+m, n = len(a), len(a[0])
+dists = [[1 for _ in range(n)] for _ in range(m)]
+
+for row in iter_rows(m, n):
+    
+    stack = []
+    
+    for i, j in row:
+        
+        while stack and stack[-1][0] < a[i][j]:
+            stack.pop()
+        
+        dists[i][j] *= j - (stack[-1][1] if stack else -1)
+        stack.append((a[i][j], j))
+
+dists
