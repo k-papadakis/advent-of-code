c0c48fc615c2a0195ba8a71a206453f7cbe58387
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2022-12-09 10:49:02 +0200

    day_08 part2 with monotonic stack

--- day_08/solution.py
+++ day_08/solution.py
@@ -1,8 +1,3 @@
-# %%
-
-import math
-
-
 def read_data(input_file):
     with open(input_file) as f:
         return [list(map(int, line)) for line in f.read().splitlines()]
@@ -28,77 +23,80 @@ def iter_cols_reversed(m, n):
         yield ((i, j) for i in reversed(range(m)))
 
 
-def visibility(row_or_col_iter, array, visible):
-    for indices in row_or_col_iter:
-        max_so_far = -math.inf
-        for i, j in indices:
-            if array[i][j] > max_so_far:
-                max_so_far = array[i][j]
-                visible[i][j] = True
-
-
 def part1(array):
     m, n = len(array), len(array[0])
     visible = [[False for _ in range(n)] for _ in range(m)]
 
     for gen in iter_rows(m, n), iter_cols(m, n), iter_rows_reversed(m, n), iter_cols_reversed(m, n):
-        visibility(gen, array, visible)
+        for indices in gen:
+            max_so_far = float('-Infinity')
+            for i, j in indices:
+                if array[i][j] > max_so_far:
+                    max_so_far = array[i][j]
+                    visible[i][j] = True
 
     return sum(map(sum, visible))
 
 
-# def find_visibility(array, i, j, direction):
+def part2(array):
+    m, n = len(array), len(array[0])
+    dists = [[1 for _ in range(n)] for _ in range(m)]
 
-#     m, n = len(array), len(array[0])
-#     n_steps = 0
-#     x, y = i, j
+    for row in iter_rows(m, n):
 
-#     while (x := x + direction[0]) in range(m) and (y := y + direction[1]) in range(n):
-#         n_steps += 1
-#         if array[x][y] >= array[i][j]:
-#             break
+        stack = []
 
-#     return n_steps
+        for i, j in row:
+
+            while stack and stack[-1][0] < array[i][j]:
+                stack.pop()
+
+            dists[i][j] *= (j - stack[-1][1]) if stack else j
+            stack.append((array[i][j], j))
+
+    for col in iter_cols(m, n):
+
+        stack = []
+
+        for i, j in col:
+
+            while stack and stack[-1][0] < array[i][j]:
+                stack.pop()
+
+            dists[i][j] *= (i - stack[-1][1]) if stack else i
+            stack.append((array[i][j], i))
+
+    for reversed_row in iter_rows_reversed(m, n):
+
+        stack = []
+
+        for i, j in reversed_row:
+
+            while stack and stack[-1][0] < array[i][j]:
+                stack.pop()
+
+            dists[i][j] *= (stack[-1][1] - j) if stack else n - 1 - j
+            stack.append((array[i][j], j))
+
+    for reversed_col in iter_cols_reversed(m, n):
+
+        stack = []
+
+        for i, j in reversed_col:
+
+            while stack and stack[-1][0] < array[i][j]:
+                stack.pop()
+
+            dists[i][j] *= (stack[-1][1] - i) if stack else m - 1 - i
+            stack.append((array[i][j], i))
+
+    return max(map(max, dists))
 
 
-# def part2(array):
-#     m, n = len(array), len(array[0])
-#     directions = (0, 1), (1, 0), (-1, 0), (0, -1)
-
-#     ret = max(
-#         math.prod(find_visibility(array, i, j, direction)
-#                   for direction in directions)
-#         for i in range(m)
-#         for j in range(n)
-#     )
-
-#     return ret
+def main():
+    a = read_data('input.txt')
+    print(f'part1: {part1(a)}\npart2: {part2(a)}')
 
 
-# def main():
-#     a = read_data('input.txt')
-#     print(f'part1: {part1(a)}\npart2: {part2(a)}')
-
-
-# if __name__ == '__main__':
-#     main()
-
-
-# %%
-a = read_data('mini.txt')
-m, n = len(a), len(a[0])
-dists = [[1 for _ in range(n)] for _ in range(m)]
-
-for row in iter_rows(m, n):
-    
-    stack = []
-    
-    for i, j in row:
-        
-        while stack and stack[-1][0] < a[i][j]:
-            stack.pop()
-        
-        dists[i][j] *= j - (stack[-1][1] if stack else -1)
-        stack.append((a[i][j], j))
-
-dists
+if __name__ == '__main__':
+    main()
