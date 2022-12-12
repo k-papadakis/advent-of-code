ec71b54e8f7712f82b894340218d78647fb3b1a3
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2022-12-12 13:08:01 +0200

    Completed day_09

--- day_09/solution.py
+++ day_09/solution.py
@@ -1,14 +1,9 @@
-# %%
 def read_data(input_file):
     dirmap = {'L': (0, -1), 'R': (0, 1), 'U': (1, 0), 'D': (-1, 0)}
     with open(input_file) as f:
         for line in f:
             direction, n_steps = line.split()
             yield dirmap[direction], int(n_steps)
-
-
-def l1(v):
-    return abs(v[0]) + abs(v[1])
 
 
 def add(u, v):
@@ -19,61 +14,47 @@ def subtract(u, v):
     return u[0] - v[0], u[1] - v[1]
 
 
-def arg_non_zero(v):
-    return 0 if v[0] != 0 else 1
+def l1(u, v):
+    return abs(u[0] - v[0]) + abs(u[1] - v[1])
 
 
-def move_follower(leader, follower, direction):
-    relative_h_pos = subtract(leader, follower)
+def find_follower_direction(leader, follower, leader_direction):
 
-    if l1(relative_h_pos) == 2:
-        idx = arg_non_zero(direction)
-        if relative_h_pos[idx] == direction[idx]:
-            follower = leader
-    else:
-        idx = arg_non_zero(direction)
-        if relative_h_pos[idx] == direction[idx]:
-            follower = add(follower, direction)
+    directions = {(-1, 0), (0, -1), (1, 0), (0, 1), (1, 1), (1, -1), (-1, 1), (-1, -1), (0, 0)}
 
-    return follower
+    leader_next = add(leader, leader_direction)
+
+    if subtract(leader_next, follower) in directions:
+        return 0, 0
+
+    direction = min(directions, key=lambda direction: l1(leader_next, add(follower, direction)))
+    return direction
 
 
-def part1(data):
-    head = tail = 0, 0
-    visited = {tail}
+def simulate(data, n_knots):
+    knots = [(0, 0)] * n_knots
+    visited = {knots[-1]}
 
     for direction, n_steps in data:
         for _ in range(n_steps):
-            tail = move_follower(tail, head, direction)
-            head = add(head, direction)
-            visited.add(tail)
 
-    return len(visited)
-
-
-def part2(data):
-    knots = [(0, 0)] * 10
-    visited = {knots[-1]}
-
-    for head_direction, n_steps in data:
-        for _ in range(n_steps):
-            direction = head_direction
-            print(f'Moving head by {direction}')
+            directions = [direction]
 
             for i in range(len(knots) - 1):
-                follower_old = knots[i + 1]
-                knots[i + 1] = move_follower(knots[i], knots[i + 1], direction)
-                knots[i] = add(knots[i], direction)
-                direction = subtract(knots[i + 1], follower_old)
-                print(direction)
-                
+                directions.append(find_follower_direction(knots[i], knots[i + 1], directions[-1]))
+
+            knots = list(map(add, knots, directions))
+
             visited.add(knots[-1])
 
-
-    print(visited)
     return len(visited)
 
 
-part2(read_data('mini.txt'))
+def main():
+    part1 = simulate(read_data('input.txt'), 2)
+    part2 = simulate(read_data('input.txt'), 10)
+    print(f'part1: {part1}\npart2: {part2}')
 
-# %%
+
+if __name__ == '__main__':
+    main()
