9ea2f076a9a6bd7f70038cf4da5b563062be4876
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2022-12-12 17:45:36 +0200

    Completed day_10

--- day_10/solution.py
+++ day_10/solution.py
@@ -7,26 +7,53 @@ def read_data(input_file):
             else:  # line.startswith('add')
                 yield int(line.split()[1])
 
-def part1(ops):
+
+def part1(vals):
     cycle_start, cycle_end = None, 1
     x = 1
     res = 0
 
-    for op in ops:
-        if op is None:
+    for val in vals:
+        if val is None:
             cycle_start, cycle_end = cycle_end, cycle_end + 1
         else:
             cycle_start, cycle_end = cycle_end, cycle_end + 2
-            
+
         for cycle in range(20, 221, 40):
             if cycle_start <= cycle < cycle_end:
                 res += cycle * x
-        
-        if op is not None:
-            x += op
-        
+
+        if val is not None:
+            x += val
+
     return res
 
 
-print(part1(read_data('input.txt')))
-    
+def part2(vals):
+    monitor = ['.'] * 240
+    sprite = 0
+    cycle_start, cycle_end = None, 0
+
+    for val in vals:
+
+        if val is None:
+            cycle_start, cycle_end = cycle_end, cycle_end + 1
+        else:
+            cycle_start, cycle_end = cycle_end, cycle_end + 2
+
+        for k in range(cycle_start, cycle_end):
+            if sprite <= k % 40 < sprite + 3:
+                monitor[k] = '#'
+
+        if val is not None:
+            sprite += val
+
+    return '\n'.join(''.join(monitor[i * 40 + j] for j in range(40)) for i in range(6))
+
+
+def main():
+    print(f'part1: {part1(read_data("mini.txt"))}\npart2:\n{part2(read_data("input.txt"))}')
+
+
+if __name__ == '__main__':
+    main()
