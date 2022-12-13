079f55b8ac38a815046ec0662d5eee9ad1aed1ed
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2022-12-13 02:31:10 +0200

    Completed day_12

--- day_12/solution.py
+++ day_12/solution.py
@@ -1,23 +1,25 @@
 from collections import deque
 
 
-def read_data(input_file) -> list[list[int]]:
+def read_data(input_file) -> tuple[list[list[str]], tuple[int, int], tuple[int, int]]:
     with open(input_file) as f:
-        grid = f.read().splitlines()
+        grid_rows = f.read().splitlines()
 
-    flattened = ''.join(grid)
-    n_cols = len(grid[0])
+    flattened = ''.join(grid_rows)
+    n_cols = len(grid_rows[0])
 
     source = divmod(flattened.index('S'), n_cols)
     target = divmod(flattened.index('E'), n_cols)
 
+    grid = list(map(list, grid_rows))
+
+    grid[source[0]][source[1]] = 'a'
+    grid[target[0]][target[1]] = 'z'
+
     return grid, source, target
 
 
-def bfs(grid, source, target):
-    elevation = {c: i for i, c in enumerate('abcdefghijklmnopqrstuvwxyz')}
-    elevation['S'] = elevation['a']
-    elevation['E'] = elevation['z']
+def bfs(grid: list[list[str]], source: tuple[int, int], target: str | tuple[int, int], reverse: bool) -> int:
 
     directions = (-1, 0), (0, -1), (1, 0), (0, 1)
     m, n = len(grid), len(grid[0])
@@ -27,14 +29,19 @@ def bfs(grid, source, target):
     level = -1
 
     while queue:
+
         level += 1
 
         for _ in range(len(queue)):
 
             cur = queue.popleft()
 
-            if cur == target:
-                return level
+            if isinstance(target, tuple):
+                if cur == target:
+                    return level
+            else:
+                if grid[cur[0]][cur[1]] == target:
+                    return level
 
             if cur in visited:
                 continue
@@ -42,13 +49,24 @@ def bfs(grid, source, target):
 
             for direction in directions:
                 neighbor = cur[0] + direction[0], cur[1] + direction[1]
-                if (
-                    0 <= neighbor[0] < m and 0 <= neighbor[1] < n and
-                    elevation[grid[neighbor[0]][neighbor[1]]] - elevation[grid[cur[0]][cur[1]]] <= 1
-                ):
-                    queue.append(neighbor)
+                if 0 <= neighbor[0] < m and 0 <= neighbor[1] < n:
+
+                    diff = ord(grid[neighbor[0]][neighbor[1]]) - ord(grid[cur[0]][cur[1]])
+                    if reverse:
+                        diff *= -1
+
+                    if diff <= 1:
+                        queue.append(neighbor)
 
     return -1
 
 
-print(bfs(*read_data('input.txt')))
+def main():
+    grid, source, target = read_data('input.txt')
+    part1 = bfs(grid, source, target, False)
+    part2 = bfs(grid, target, 'a', True)
+    print(f'part1: {part1}\npart2: {part2}')
+
+
+if __name__ == '__main__':
+    main()
