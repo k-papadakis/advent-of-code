5ce39500e0a4c5b05bdcc13f3fa86e91b9d839e9
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2022-12-13 16:16:45 +0200

    Completed day_13

--- day_13/solution.py
+++ day_13/solution.py
@@ -1,4 +1,6 @@
-from itertools import takewhile
+from itertools import chain, takewhile
+from functools import cmp_to_key
+from math import prod
 
 
 def read_data(input_file):
@@ -10,9 +12,9 @@ def read_data(input_file):
 def compare(a, b):
     if isinstance(a, int) and isinstance(b, int):
         if a < b:
-            return 1
-        elif a > b:
             return -1
+        elif a > b:
+            return 1
         else:
             return 0
 
@@ -27,16 +29,27 @@ def compare(a, b):
             return t
 
     if len(a) < len(b):
-        return 1
-    elif len(a) > len(b):
         return -1
+    elif len(a) > len(b):
+        return 1
     else:
         return 0
 
 
+def part1(data):
+    return sum(i for i, (a, b) in enumerate(data, 1) if compare(a, b) != 1)
+
+
+def part2(data):
+    extras = ([[2]], [[6]])
+    data_cat = chain(extras, chain.from_iterable(data))
+    data_cat_increasing = sorted(data_cat, key=cmp_to_key(compare))
+    return prod(map(lambda i: i + 1, map(data_cat_increasing.index, extras)))
+
+
 def main():
-    part1 = sum(i for i, (a, b) in enumerate(read_data('input.txt'), 1) if compare(a, b) != -1)
-    print(f'part1: {part1}')
+    input_file = 'input.txt'
+    print(f'part1: {part1(read_data(input_file))}\npart2: {part2(read_data(input_file))}')
 
 
 if __name__ == '__main__':
