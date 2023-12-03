53fca8ee66008fadc1b7d3ec36b528f04ab50141
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2023-12-03 04:29:07 +0200

    Minor refactor of 2023 days 1 and 2

--- 2023/day_01/solution.py
+++ 2023/day_01/solution.py
@@ -1,44 +1,40 @@
 import re
-from typing import Callable
+from collections.abc import Callable, Generator
 
-WORDS = (
-    "zero",
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-)
+WORDS = ("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+word_to_digit = {x: str(i) for i, x in enumerate(WORDS)}
 
-SUBPATTERN_1 = r"\d"
+pattern_template = r"(?:.*?({subpattern}).*({subpattern}).*?$)|(?:.*?({subpattern}))"
 
-WORDS_JOINED = "|".join(WORDS)
-SUBPATTERN_2 = rf"\d|{WORDS_JOINED}"
-
-PATTERN_TEMPLATE = r"(?:.*?({subpattern}).*({subpattern}).*?$)|(?:.*?({subpattern}))"
-
-PATTERN_1 = re.compile(PATTERN_TEMPLATE.format(subpattern=SUBPATTERN_1))
-PATTERN_2 = re.compile(PATTERN_TEMPLATE.format(subpattern=SUBPATTERN_2))
-
-WORD_TO_DIGIT = {x: str(i) for i, x in enumerate(WORDS)}
+pattern_1 = pattern_template.format(subpattern=r"\d")
+pattern_2 = pattern_template.format(subpattern=rf"\d|{'|'.join(WORDS)}")
 
 
-def read_data():
+def _assert_match(match: re.Match[str] | None) -> re.Match[str]:
+    if not match:
+        raise ValueError("Could not match pattern.")
+    return match
+
+
+def read_data() -> Generator[str, None, None]:
     with open("input.txt") as f:
         for line in f:
             yield line.rstrip("\n")
 
 
-def number_constructor(pattern: re.Pattern) -> Callable[[str], int]:
-    def construct_number(s: str) -> int:
-        x, y, z = pattern.match(s).groups()
-        d1, d2 = (z, z) if z is not None else (x, y)
+def number_constructor(pattern: str | re.Pattern[str]) -> Callable[[str], int]:
+    pattern = re.compile(pattern)
 
-        num1, num2 = map(int, (WORD_TO_DIGIT.get(g, g) for g in (d1, d2)))
+    def construct_number(s: str) -> int:
+        match _assert_match(pattern.match(s)).groups():
+            case None, None, str(z):
+                d1 = d2 = z
+            case str(x), str(y), None:
+                d1, d2 = x, y
+            case _:
+                raise ValueError("Invalid regex match.")
+
+        num1, num2 = map(int, (word_to_digit.get(g, g) for g in (d1, d2)))
 
         num = 10 * num1 + num2
         return num
@@ -47,8 +43,8 @@ def number_constructor(pattern: re.Pattern) -> Callable[[str], int]:
 
 
 def main() -> None:
-    part_1 = sum(map(number_constructor(PATTERN_1), read_data()))
-    part_2 = sum(map(number_constructor(PATTERN_2), read_data()))
+    part_1 = sum(map(number_constructor(pattern_1), read_data()))
+    part_2 = sum(map(number_constructor(pattern_2), read_data()))
 
     print(f"{part_1 = }, {part_2 = }")
 
