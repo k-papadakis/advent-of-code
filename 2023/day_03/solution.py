8888a6b645e38c2647a8196a9ea92fc973b00f50
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2023-12-03 16:13:27 +0200

    Finished 2023 day_03

--- 2023/day_03/solution.py
+++ 2023/day_03/solution.py
@@ -1,54 +1,113 @@
-# %%
+import math
+from collections import defaultdict
+from collections.abc import Generator
+
+type Coordinate = tuple[int, int]
+
+
 def read_input() -> list[list[str]]:
     with open("input.txt") as f:
         return [list(line) for line in f.read().splitlines()]
-
-
-arr = read_input()
-
-m = len(arr)
-n = len(arr[0])
 
 
 def issymbol(s: str) -> bool:
     return len(s) == 1 and s != "." and not s.isdigit()
 
 
-# %%
-res: int = 0
-start: tuple[int, int] | None = None
-bordering: bool = False
+def neighbors(arr: list[list], i: int, j: int) -> Generator[Coordinate, None, None]:
+    m = len(arr)
+    n = len(arr[0])
+    for k, l in (
+        (i - 1, j),
+        (i + 1, j),
+        (i, j - 1),
+        (i, j + 1),
+        (i + 1, j + 1),
+        (i - 1, j - 1),
+        (i - 1, j + 1),
+        (i + 1, j - 1),
+    ):
+        if 0 <= k < m and 0 <= l < n:
+            yield k, l
 
-for i in range(m):
-    if start and bordering:
-        res += int("".join(arr[start[0]][start[1] :]))
-    start = None
-    bordering = False
 
-    for j in range(n):
-        if arr[i][j].isdigit():
-            if not start:
-                start = i, j
+def list_to_int(lst: list[str]) -> int:
+    return int("".join(lst))
 
-            if not bordering:
-                for k, l in (
-                    (i - 1, j),
-                    (i + 1, j),
-                    (i, j - 1),
-                    (i, j + 1),
-                    (i + 1, j + 1),
-                    (i - 1, j - 1),
-                    (i - 1, j + 1),
-                    (i + 1, j - 1),
-                ):
-                    if 0 <= k < m and 0 <= l < n:
-                        if issymbol(arr[k][l]):
-                            bordering = True
-        else:
-            if start and bordering:
-                res += int("".join(arr[start[0]][start[1] : j]))
+
+def get_part_nums(arr: list[list[str]]) -> Generator[int, None, None]:
+    m = len(arr)
+    n = len(arr[0])
+
+    start: Coordinate | None = None
+    borders_symbol: bool = False
+
+    for i in range(m):
+        if start and borders_symbol:
+            yield list_to_int(arr[start[0]][start[1] :])
+        start = None
+        borders_symbol = False
+
+        for j in range(n):
+            if arr[i][j].isdigit():
+                if not start:
+                    start = i, j
+
+                if not borders_symbol:
+                    borders_symbol = any(
+                        issymbol(arr[k][l]) for k, l in neighbors(arr, i, j)
+                    )
+
+            else:
+                if start and borders_symbol:
+                    yield list_to_int(arr[start[0]][start[1] : j])
+                start = None
+                borders_symbol = False
+
+
+def get_gears(arr: list[list[str]]) -> dict[Coordinate, tuple[int, int]]:
+    m = len(arr)
+    n = len(arr[0])
+
+    start: Coordinate | None = None
+    bordering_stars: set[Coordinate] = set()
+    stars: dict[int, list[Coordinate]] = defaultdict(list)
+
+    for i in range(m):
+        if start and bordering_stars:
+            number = list_to_int(arr[start[0]][start[1] :])
+
+            for star in bordering_stars:
+                stars[star].append(number)
+
+            bordering_stars.clear()
             start = None
-            bordering = False
-print(res)
 
-# %%
+        for j in range(n):
+            if arr[i][j].isdigit():
+                if not start:
+                    start = i, j
+                bordering_stars.update(
+                    (k, l) for k, l in neighbors(arr, i, j) if arr[k][l] == "*"
+                )
+            else:
+                if start and bordering_stars:
+                    number = list_to_int(arr[start[0]][start[1] : j])
+                    for star in bordering_stars:
+                        stars[star].append(number)
+
+                start = None
+                bordering_stars.clear()
+
+    gears = {k: tuple(v) for k, v in stars.items() if len(v) == 2}
+    return gears
+
+
+def main() -> None:
+    part_1 = sum(get_part_nums(read_input()))
+    part_2 = sum(x * y for (x, y) in get_gears(read_input()).values())
+    print(f"{part_1 = }, {part_2 = }")
+
+
+if __name__ == "__main__":
+    main()
