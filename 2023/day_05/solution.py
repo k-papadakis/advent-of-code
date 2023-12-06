31e58220debe98798333a2fcf182eab5c2007e59
Konstantinos Papadakis <kpapadakis@everypay.gr>
2023-12-06 12:05:20 +0200

    Refactored 2023 day_05 part_1

--- 2023/day_05/solution.py
+++ 2023/day_05/solution.py
@@ -1,92 +1,49 @@
-import re
-from dataclasses import dataclass
+type IntervalCollection = list[tuple[int, int]]
+type Almanac = list[tuple[IntervalCollection, IntervalCollection]]
 
 
-@dataclass
-class RangeMap:
-    destination_range_start: int
-    source_range_start: int
-    range_length: int
-
-    def __contains__(self, key: int) -> bool:
-        return 0 <= (key - self.source_range_start) < self.range_length
-
-    def __getitem__(self, key: int) -> int:
-        if key in self:
-            return self.destination_range_start + (key - self.source_range_start)
-        else:
-            raise KeyError(key)
-
-    @classmethod
-    def from_string(cls, range_map_str: str) -> "RangeMap":
-        return cls(*map(int, range_map_str.split()))
+def intervals_map(p: IntervalCollection, q: IntervalCollection, x: int):
+    for u, v in zip(p, q, strict=True):
+        if u[0] <= x <= u[1]:
+            return v[0] + (x - u[0])
+    return x
 
 
-@dataclass
-class RangeMapCollection:
-    source_category: str
-    target_category: str
-    range_maps: list[RangeMap]
-
-    def __getitem__(self, key: int) -> int:
-        for range_map in self.range_maps:
-            if key in range_map:
-                return range_map[key]
-        return key
-
-    @classmethod
-    def from_string(cls, range_map_collection_str: str) -> "RangeMapCollection":
-        match = re.match(
-            r"(?P<source_category>.*?)-to-(?P<target_category>.*?) map:\n(?P<range_maps>.*)",
-            range_map_collection_str,
-            re.DOTALL,
-        )
-        if not match:
-            raise ValueError("Could not match pattern.")
-
-        range_maps = [
-            RangeMap.from_string(range_map_str)
-            for range_map_str in match["range_maps"].splitlines()
-        ]
-        return cls(match["source_category"], match["target_category"], range_maps)
+def almanac_map(almanac: Almanac, x: int) -> int:
+    res = x
+    for p, q in almanac:
+        res = intervals_map(p, q, res)
+    return res
 
 
-@dataclass
-class Almanac:
-    range_map_collections: list[RangeMapCollection]
-
-    def __getitem__(self, key: int) -> int:
-        res = key
-        for range_map_collection in self.range_map_collections:
-            res = range_map_collection[res]
-        return res
-
-    @classmethod
-    def from_string(cls, almanac_str: str) -> "Almanac":
-        almanac = cls(
-            [
-                RangeMapCollection.from_string(range_map_collection_str)
-                for range_map_collection_str in almanac_str.split("\n\n")
-            ]
-        )
-        return almanac
-
-
-def read_input() -> tuple[list[int], Almanac]:
+def read_input() -> (
+    tuple[list[int], list[tuple[IntervalCollection, IntervalCollection]]]
+):
     with open("input.txt") as f:
         seeds_str = f.readline()
         f.readline()
         almanac_str = f.read()
 
     seeds = list(map(int, seeds_str.lstrip("seeds:").split()))
-    almanac = Almanac.from_string(almanac_str)
+
+    almanac: Almanac = []
+    for chunk in almanac_str.split("\n\n"):
+        c1: IntervalCollection = []
+        c2: IntervalCollection = []
+        for line in chunk.splitlines()[1:]:
+            dest_start, source_start, length = map(int, line.split())
+            c1.append((source_start, source_start + length))
+            c2.append((dest_start, dest_start + length))
+
+        almanac.append((c1, c2))
 
     return seeds, almanac
 
 
 def main() -> None:
     seeds, almanac = read_input()
-    part_1 = min(almanac[seed] for seed in seeds)
+    part_1 = min(almanac_map(almanac, x) for x in seeds)
+
     print(f"{part_1 = }")
 
 
