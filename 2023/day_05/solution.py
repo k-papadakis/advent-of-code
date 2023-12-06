d9e339ac30d1aec9b63318b60ac2ecab2cbd2718
Konstantinos Papadakis <kpapadakis@everypay.gr>
2023-12-06 20:05:02 +0200

    Completed 2023 day_05

--- 2023/day_05/solution.py
+++ 2023/day_05/solution.py
@@ -5,66 +5,73 @@ type Almanac = list[tuple[IntervalCollection, IntervalCollection]]
 
 def union(p: IntervalCollection) -> IntervalCollection:
     p_sorted = sorted(p)
+
     res = [p_sorted[0]]
+
     for x, y in p_sorted[1:]:
         a, b = res[-1]
+
         if x <= b:
-            res[-1] = (a, y)
+            res[-1] = (a, max(b, y))
         else:
             res.append((x, y))
+
     return res
 
 
 def partition(
     p: IntervalCollection, partitioner: Interval
 ) -> tuple[IntervalCollection, IntervalCollection]:
-    inside: IntervalCollection = []
-    outside: IntervalCollection = []
+    intersections: IntervalCollection = []
+    complements: IntervalCollection = []
+
     a, b = partitioner
+
     for x, y in p:
         s, t = max(a, x), min(b, y)
 
         if s <= t:
-            inside.append((s, t))  # intersection
+            intersections.append((s, t))  # intersection
             if x < s:
-                outside.append((x, s - 1))  # left remainder
+                complements.append((x, s - 1))  # left complement
             if t < y:
-                outside.append((t + 1, y))  # right remainder
+                complements.append((t + 1, y))  # right complement
         else:
-            outside.append((x, y))
+            complements.append((x, y))  # complement
 
-    return inside, outside
+    return intersections, complements
 
 
 def shift(u: Interval, v: Interval, x: int) -> int:
     return v[0] + (x - u[0])
 
 
-def map_single(p: IntervalCollection, q: IntervalCollection, x: int) -> int:
+def map_int(p: IntervalCollection, q: IntervalCollection, x: int) -> int:
     for u, v in zip(p, q, strict=True):
         if u[0] <= x <= u[1]:
             return shift(u, v, x)
     return x
 
 
-def map_ranges(
+def almanac_map_int(almanac: Almanac, x: int) -> int:
+    res = x
+    for p, q in almanac:
+        res = map_int(p, q, res)
+    return res
+
+
+def map_interval_collections(
     p: IntervalCollection, q: IntervalCollection, intervals: IntervalCollection
 ) -> IntervalCollection:
     res: IntervalCollection = []
+
     for u, v in zip(p, q):
-        # TODO: union here
         intersected, intervals = partition(intervals, u)
         res.extend((shift(u, v, x), shift(u, v, y)) for x, y in intersected)
+
     res.extend(intervals)
-    res = union(res)
-    return res
 
-
-def almanac_map_single(almanac: Almanac, x: int) -> int:
-    res = x
-    for p, q in almanac:
-        res = map_single(p, q, res)
-    return res
+    return union(res)
 
 
 def almanac_map_range(
@@ -72,7 +79,7 @@ def almanac_map_range(
 ) -> IntervalCollection:
     res = intervals
     for p, q in almanac:
-        res = map_ranges(p, q, res)
+        res = map_interval_collections(p, q, res)
     return res
 
 
@@ -83,7 +90,10 @@ def read_input() -> tuple[list[int], IntervalCollection, Almanac]:
         almanac_str = f.read()
 
     seeds = list(map(int, seeds_str.lstrip("seeds:").split()))
-    seed_ranges = list(zip(seeds[:-1:2], seeds[1::2], strict=True))
+    seed_ranges = [
+        (start, start + length)
+        for start, length in zip(seeds[:-1:2], seeds[1::2], strict=True)
+    ]
 
     almanac: Almanac = []
     for chunk in almanac_str.split("\n\n"):
@@ -101,13 +111,11 @@ def read_input() -> tuple[list[int], IntervalCollection, Almanac]:
 
 
 def main() -> None:
-    # TODO: Sort the intervals
     seeds, seed_ranges, almanac = read_input()
 
-    part_1 = min(almanac_map_single(almanac, x) for x in seeds)
+    part_1 = min(almanac_map_int(almanac, x) for x in seeds)
     part_2 = min(a for a, _ in almanac_map_range(almanac, seed_ranges))
     print(f"{part_1 = }, {part_2 = }")
-    print(almanac_map_range(almanac, seed_ranges))
 
 
 if __name__ == "__main__":
