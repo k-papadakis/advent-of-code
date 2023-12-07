b071b0c561739a6395a9f08460feb53212084697
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2023-12-07 18:30:06 +0200

    Refactored and added description.

--- 2023/day_07/solution.py
+++ 2023/day_07/solution.py
@@ -11,52 +11,42 @@ def read_input(path: str) -> tuple[list[str], list[int]]:
     return hands, bids
 
 
-def hands_comparison_key_1(hand: str):
-    card_priority = {x: i for i, x in enumerate("23456789TJQKA")}
+def create_hands_key(use_j: bool):
+    card_priority = {
+        x: i for i, x in enumerate("J23456789TQKA" if use_j else "23456789TJQKA")
+    }
 
-    card_counts = sorted(Counter(hand).values(), reverse=True)
+    def hands_key(hand: str) -> tuple[list[int], list[int]]:
+        c = Counter(hand)
 
-    hand_priority = [card_priority[card] for card in hand]
+        if use_j and "J" in c and len(c) > 1:
+            js = c.pop("J")
+            c[c.most_common(1)[0][0]] += js
 
-    return card_counts, hand_priority
+        card_counts = sorted(c.values(), reverse=True)
+        hand_priority = [card_priority[card] for card in hand]
 
+        return card_counts, hand_priority
 
-def hands_comparison_key_2(hand: str):
-    card_priority = {x: i for i, x in enumerate("J23456789TQKA")}
-
-    c = Counter(hand)
-    if "J" in c and len(c) > 1:
-        js = c.pop("J")
-        c[c.most_common(1)[0][0]] += js
-    card_counts = sorted(c.values(), reverse=True)
-
-    hand_priority = [card_priority[card] for card in hand]
-
-    return card_counts, hand_priority
+    return hands_key
 
 
-def total_winnings(hands, bids, hands_comparison_key):
+def total_winnings(hands: list[str], bids: list[int], hands_key) -> int:
     res = sum(
         i * bid
         for i, (_, bid) in enumerate(
-            sorted(
-                zip(
-                    hands,
-                    bids,
-                ),
-                key=lambda t: hands_comparison_key(t[0]),
-            ),
-            1,
+            sorted(zip(hands, bids), key=lambda t: hands_key(t[0])),
+            start=1,
         )
     )
     return res
 
 
-def main():
+def main() -> None:
     hands, bids = read_input("input.txt")
 
-    part_1 = total_winnings(hands, bids, hands_comparison_key_1)
-    part_2 = total_winnings(hands, bids, hands_comparison_key_2)
+    part_1 = total_winnings(hands, bids, create_hands_key(use_j=False))
+    part_2 = total_winnings(hands, bids, create_hands_key(use_j=True))
 
     print(f"{part_1 = }, {part_2 = }")
 
