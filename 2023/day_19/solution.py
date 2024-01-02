0d72042f52d1d519781b8003f9398728dba2114f
Konstantinos Papadakis <kpapadakis@everypay.gr>
2024-01-02 17:36:47 +0200

    Refactored 2023 day_19 part_1

--- 2023/day_19/solution.py
+++ 2023/day_19/solution.py
@@ -1,4 +1,3 @@
-from operator import gt, lt
 import re
 from dataclasses import dataclass
 
@@ -9,33 +8,71 @@ class Part:
     m: int
     a: int
     s: int
-    
+
     def sum(self) -> int:
         return self.x + self.m + self.a + self.s
 
 
-@dataclass(slots=True, frozen=True)
-class Workflow:
-    name: str
-    logic: str
+MIN = 1
+MAX = 4_000
 
-    def __call__(self, part: Part) -> str:
-        *head, final = self.logic.split(",")
+
+@dataclass(slots=True, frozen=True)
+class PartRange:
+    x: tuple[int, int] = (MIN, MAX)
+    m: tuple[int, int] = (MIN, MAX)
+    a: tuple[int, int] = (MIN, MAX)
+    s: tuple[int, int] = (MIN, MAX)
+
+    def __len__(self) -> int:
+        return (
+            (self.x[1] - self.x[0] + 1)
+            * (self.m[1] - self.m[0] + 1)
+            * (self.a[1] - self.a[0] + 1)
+            * (self.s[1] - self.s[0] + 1)
+        )
+
+    def __contains__(self, part: Part) -> bool:
+        return (
+            self.x[0] <= part.x <= self.x[1]
+            and self.m[0] <= part.m <= self.m[1]
+            and self.a[0] <= part.a <= self.a[1]
+            and self.s[0] <= part.s <= self.s[1]
+        )
+
+
+class Workflow:
+    def __init__(self, name: str, logic_str: str) -> None:
+        self.name = name
+        self.logic_str = logic_str
+        self.logic: list[tuple[PartRange, str]] = []
+
         pattern = re.compile(r"(x|m|a|s)(>|<)(\d+):(\w+)")
+        *head, final = self.logic_str.split(",")
 
         for h in head:
             m = pattern.match(h)
             if not m:
                 raise ValueError("Could not match pattern.")
+
             t, op_str, val_str, r = m.groups()
-            op = gt if op_str == ">" else lt
             val = int(val_str)
 
-            if op(getattr(part, t), val):
-                return r
+            if op_str == ">":
+                rn = PartRange(**{t: (val + 1, MAX)})
+            else:
+                rn = PartRange(**{t: (MIN, val - 1)})
 
-        return final
-    
+            self.logic.append((rn, r))
+
+        self.logic.append((PartRange(), final))
+
+    def process_part(self, part: Part) -> str:
+        for rn, r in self.logic:
+            if part in rn:
+                return r
+        return "error"
+
 
 def read_input(path: str) -> tuple[dict[str, Workflow], list[Part]]:
     with open(path) as f:
@@ -44,12 +81,12 @@ def read_input(path: str) -> tuple[dict[str, Workflow], list[Part]]:
     workflows_str, parts_str = s.split("\n\n")
 
     workflow_pattern = re.compile(r"(?P<name>.*?)\{(?P<logic>.*?)\}")
-    workflows: dict[str, Workflow] = {}
+    system: dict[str, Workflow] = {}
     for workflow_str in workflows_str.splitlines():
         m = workflow_pattern.match(workflow_str)
         if not m:
             raise ValueError("Could not match pattern.")
-        workflows[m["name"]] = Workflow(m["name"], m["logic"])
+        system[m["name"]] = Workflow(m["name"], m["logic"])
 
     part_pattern = re.compile(r"\{x=(\d+),m=(\d+),a=(\d+),s=(\d+)\}")
     parts: list[Part] = []
@@ -59,23 +96,20 @@ def read_input(path: str) -> tuple[dict[str, Workflow], list[Part]]:
             raise ValueError("Could not match pattern.")
         parts.append(Part(*map(int, m.groups())))
 
-    return workflows, parts
+    return system, parts
 
 
-
-    
-def accept(part: Part, workflows: dict[str, Workflow]) -> bool:
+def accepts(system: dict[str, Workflow], part: Part) -> bool:
     cur = "in"
     while True:
-        cur = workflows[cur](part)
+        cur = system[cur].process_part(part)
         if cur == "R":
             return False
         if cur == "A":
             return True
-        
 
-        
+
 path = "input.txt"
 workflows, parts = read_input(path)
-res = sum(part.sum() for part in parts if accept(part, workflows))
+res = sum(part.sum() for part in parts if accepts(workflows, part))
 print(res)
