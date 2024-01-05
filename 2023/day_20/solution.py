7f078260c298af5013e7b2ddebcc4da6b502ad10
Konstantinos Papadakis <kpapadakis@everypay.gr>
2024-01-05 15:04:48 +0200

    Completed 2023 day_20

--- 2023/day_20/solution.py
+++ 2023/day_20/solution.py
@@ -1,6 +1,10 @@
+import math
 from collections import defaultdict, deque
+from copy import deepcopy
+from typing import Iterator
 
 type Digraph = dict[str, set[str]]
+type Message = tuple[str, str, bool]
 
 
 def read_input(path: str) -> tuple[Digraph, set[str], set[str]]:
@@ -61,14 +65,14 @@ def read_input(path: str) -> tuple[Digraph, set[str], set[str]]:
 #     plt.savefig("graph.png")
 
 
-def reverse_digraph(digraph: Digraph) -> Digraph:
+def reversed_digraph(digraph: Digraph) -> Digraph:
     res: Digraph = defaultdict(set)
 
     for node, targets in digraph.items():
         for target in targets:
             res[target].add(node)
 
-    return res
+    return dict(res)
 
 
 class System:
@@ -78,69 +82,99 @@ class System:
         flipflops: set[str],
         conjunctions: set[str],
     ) -> None:
-        reversed_digraph = reverse_digraph(digraph)
-
         self.digraph = digraph
+        self.reversed_digraph = reversed_digraph(digraph)
         self.flipflops = {f: False for f in flipflops}
         self.conjunctions = {
-            c: dict.fromkeys(reversed_digraph[c], False) for c in conjunctions
+            c: dict.fromkeys(self.reversed_digraph[c], False) for c in conjunctions
         }
 
         self.low_pulses: int = 0
         self.high_pulses: int = 0
 
-    def push_button(self) -> None:
-        q: deque[tuple[str, str, bool]] = deque()
-        q.append(("button", "broadcaster", False))
+    def reset(self) -> None:
+        self.__init__(self.digraph, set(self.flipflops), set(self.conjunctions))
 
-        while q:
-            parent, node, pulse = q.popleft()
+    def propagate(self, message: Message) -> Iterator[Message]:
+        parent, node, pulse = message
 
-            # print(f"{parent} -{'high' if pulse else 'low'}-> {node}")
+        if node == "broadcaster":
+            new_pulse = pulse
 
-            self.increment_pulses(pulse)
+            for target in self.digraph[node]:
+                yield node, target, new_pulse
 
-            if node == "broadcaster":
-                new_pulse = pulse
-
-                for target in self.digraph[node]:
-                    q.append((node, target, new_pulse))
-
-            elif node in self.flipflops:
-                if pulse:
-                    continue
+        elif node in self.flipflops:
+            if not pulse:
                 self.flipflops[node] = not self.flipflops[node]
                 new_pulse = self.flipflops[node]
 
                 for target in self.digraph[node]:
-                    q.append((node, target, new_pulse))
+                    yield node, target, new_pulse
 
-            elif node in self.conjunctions:
-                self.conjunctions[node][parent] = pulse
-                new_pulse = not all(self.conjunctions[node].values())
+        elif node in self.conjunctions:
+            self.conjunctions[node][parent] = pulse
+            new_pulse = not all(self.conjunctions[node].values())
 
-                for target in self.digraph[node]:
-                    q.append((node, target, new_pulse))
+            for target in self.digraph[node]:
+                yield node, target, new_pulse
 
+    def push_button(self) -> None:
+        q: deque[Message] = deque()
+        q.append(("button", "broadcaster", False))
+
+        while q:
+            message = _, _, pulse = q.popleft()
+
+            if pulse:
+                self.high_pulses += 1
             else:
-                continue
+                self.low_pulses += 1
 
-    def increment_pulses(self, pulse: bool) -> None:
-        if pulse:
-            self.high_pulses += 1
-        else:
-            self.low_pulses += 1
+            q.extend(self.propagate(message))
+
+    def min_pushes_low_rx(self) -> int:
+        """Assumes a very specific `System` structure"""
+        system = deepcopy(self)
+
+        (parent,) = system.reversed_digraph["rx"]
+        pparents = system.reversed_digraph[parent]
+
+        periods: dict[str, int] = {}
+        num_button_pushes = 0
+
+        while pparents:
+            num_button_pushes += 1
+
+            q: deque[Message] = deque()
+            q.append(("button", "broadcaster", False))
+
+            while q:
+                message = _, node, pulse = q.popleft()
+
+                if node in pparents and not pulse:
+                    pparents.remove(node)
+                    periods[node] = num_button_pushes
+                    break
+                
+                q.extend(self.propagate(message))
+
+        return math.lcm(*periods.values())
 
 
 def main():
     digraph, flipflops, conjunctions = read_input("input.txt")
-
     system = System(digraph, flipflops, conjunctions)
+
     for _ in range(1_000):
         system.push_button()
     part_1 = system.low_pulses * system.high_pulses
 
-    print(f"{part_1 = }")
+    system.reset()
+
+    part_2 = system.min_pushes_low_rx()
+
+    print(f"{part_1 = }, {part_2 = }")
 
 
 if __name__ == "__main__":
