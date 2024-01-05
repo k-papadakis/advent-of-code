16bd732457ff5c614cbd833215a8bc1089c30ed2
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2024-01-05 17:25:04 +0200

    Refactored

--- 2023/day_20/solution.py
+++ 2023/day_20/solution.py
@@ -1,19 +1,26 @@
 import math
 from collections import defaultdict, deque
 from copy import deepcopy
+from dataclasses import dataclass
 from typing import Iterator
 
 type Digraph = dict[str, set[str]]
-type Message = tuple[str, str, bool]
 
 
-def read_input(path: str) -> tuple[Digraph, set[str], set[str]]:
+@dataclass(slots=True)
+class Signal:
+    source: str
+    target: str
+    pulse: bool
+
+
+def read_input(path: str) -> tuple[Digraph, set[str], set[str], str]:
     with open(path) as f:
         lines = f.read().splitlines()
 
     digraph: Digraph = {}
-    flipflops: set[str] = set()
-    conjunctions: set[str] = set()
+    not_gates: set[str] = set()
+    nand_gates: set[str] = set()
 
     for line in lines:
         type_name, targets_str = line.split(" -> ")
@@ -24,22 +31,22 @@ def read_input(path: str) -> tuple[Digraph, set[str], set[str]]:
 
         elif type_name.startswith("%"):
             name = type_name[1:]
-            flipflops.add(name)
+            not_gates.add(name)
 
         elif type_name.startswith("&"):
             name = type_name[1:]
-            conjunctions.add(name)
+            nand_gates.add(name)
 
         else:
             raise ValueError(type_name)
 
         digraph[name] = set(targets)
 
-    return digraph, flipflops, conjunctions
+    return digraph, not_gates, nand_gates, "broadcaster"
 
 
 # def save_digraph_image(
-#     digraph: Digraph, flipflops: set[str], conjunctions: set[str]
+#     digraph: Digraph, not_gates: set[str], nand_gates: set[str], broadcaster_gate: str
 # ) -> None:
 #     import matplotlib.pyplot as plt
 #     import networkx as nx
@@ -51,11 +58,11 @@ def read_input(path: str) -> tuple[Digraph, set[str], set[str]]:
 
 #     colors = []
 #     for node in g.nodes():
-#         if node in flipflops:
+#         if node in not_gates:
 #             colors.append("yellow")
-#         elif node in conjunctions:
+#         elif node in nand_gates:
 #             colors.append("red")
-#         elif node == "broadcaster":
+#         elif node == broadcaster_gate:
 #             colors.append("blue")
 #         else:
 #             colors.append("green")
@@ -75,96 +82,122 @@ def reversed_digraph(digraph: Digraph) -> Digraph:
     return dict(res)
 
 
-class System:
+class Circuit:
     def __init__(
         self,
         digraph: Digraph,
-        flipflops: set[str],
-        conjunctions: set[str],
+        not_gates: set[str],
+        nand_gates: set[str],
+        broadcaster_gate: str,
     ) -> None:
         self.digraph = digraph
         self.reversed_digraph = reversed_digraph(digraph)
-        self.flipflops = {f: False for f in flipflops}
-        self.conjunctions = {
-            c: dict.fromkeys(self.reversed_digraph[c], False) for c in conjunctions
+        self.not_gates = {not_gate: False for not_gate in not_gates}
+        self.nand_gates = {
+            nand_gate: dict.fromkeys(self.reversed_digraph[nand_gate], False)
+            for nand_gate in nand_gates
         }
+        self.broadcaster_gate = broadcaster_gate
 
         self.low_pulses: int = 0
         self.high_pulses: int = 0
 
     def reset(self) -> None:
-        self.__init__(self.digraph, set(self.flipflops), set(self.conjunctions))
+        self.not_gates = {not_gate: False for not_gate in self.not_gates}
+        self.nand_gates = {
+            nand_gate: dict.fromkeys(self.reversed_digraph[nand_gate], False)
+            for nand_gate in self.nand_gates
+        }
+        self.low_pulses = 0
+        self.high_pulses = 0
 
-    def propagate(self, message: Message) -> Iterator[Message]:
-        parent, node, pulse = message
+    def propagate(self, signal: Signal) -> Iterator[Signal]:
+        if signal.target == self.broadcaster_gate:
+            yield from self.buffer_gate_propagate(signal)
 
-        if node == "broadcaster":
-            new_pulse = pulse
+        if signal.target in self.not_gates:
+            yield from self.not_gate_propagate(signal)
 
-            for target in self.digraph[node]:
-                yield node, target, new_pulse
+        if signal.target in self.nand_gates:
+            yield from self.nand_gate_propagate(signal)
 
-        elif node in self.flipflops:
-            if not pulse:
-                self.flipflops[node] = not self.flipflops[node]
-                new_pulse = self.flipflops[node]
+    def buffer_gate_propagate(self, signal: Signal) -> Iterator[Signal]:
+        new_source = signal.target
 
-                for target in self.digraph[node]:
-                    yield node, target, new_pulse
+        new_pulse = signal.pulse
 
-        elif node in self.conjunctions:
-            self.conjunctions[node][parent] = pulse
-            new_pulse = not all(self.conjunctions[node].values())
+        for new_target in self.digraph[signal.target]:
+            yield Signal(new_source, new_target, new_pulse)
 
-            for target in self.digraph[node]:
-                yield node, target, new_pulse
+    def not_gate_propagate(self, signal: Signal) -> Iterator[Signal]:
+        new_source = signal.target
+
+        if not signal.pulse:
+            self.not_gates[signal.target] = not self.not_gates[signal.target]
+            new_pulse = self.not_gates[signal.target]
+
+            for new_target in self.digraph[signal.target]:
+                yield Signal(new_source, new_target, new_pulse)
+
+    def nand_gate_propagate(self, signal: Signal) -> Iterator[Signal]:
+        new_source = signal.target
+
+        self.nand_gates[signal.target][signal.source] = signal.pulse
+        new_pulse = not all(self.nand_gates[signal.target].values())
+
+        for new_target in self.digraph[signal.target]:
+            yield Signal(new_source, new_target, new_pulse)
 
     def push_button(self) -> None:
-        q: deque[Message] = deque()
-        q.append(("button", "broadcaster", False))
+        signal_queue: deque[Signal] = deque()
+        signal_queue.append(Signal("button", self.broadcaster_gate, False))
 
-        while q:
-            message = _, _, pulse = q.popleft()
+        while signal_queue:
+            signal = signal_queue.popleft()
 
-            if pulse:
+            if signal.pulse:
                 self.high_pulses += 1
             else:
                 self.low_pulses += 1
 
-            q.extend(self.propagate(message))
+            signal_queue.extend(self.propagate(signal))
 
     def min_pushes_low_rx(self) -> int:
-        """Assumes a very specific `System` structure"""
+        """
+        Assumes a very specific `System` structure:
+          - rx has a single parent, which is a NAND gate.
+          - The grandparents of rx receive a low (False) pulse at regular intervals of button pushes.
+        """
         system = deepcopy(self)
 
-        (parent,) = system.reversed_digraph["rx"]
-        pparents = system.reversed_digraph[parent]
+        (rx_parent,) = system.reversed_digraph["rx"]
+        rx_grandparents = system.reversed_digraph[rx_parent]
 
-        periods: dict[str, int] = {}
-        num_button_pushes = 0
+        button_push_periods: dict[str, int] = {}
+        button_push_count = 0
 
-        while pparents:
-            num_button_pushes += 1
+        while rx_grandparents:
+            button_push_count += 1
 
-            q: deque[Message] = deque()
-            q.append(("button", "broadcaster", False))
+            signal_queue: deque[Signal] = deque()
+            signal_queue.append(Signal("button", self.broadcaster_gate, False))
 
-            while q:
-                message = _, node, pulse = q.popleft()
+            while signal_queue:
+                signal = signal_queue.popleft()
 
-                if node in pparents and not pulse:
-                    pparents.remove(node)
-                    periods[node] = num_button_pushes
+                if signal.target in rx_grandparents and not signal.pulse:
+                    rx_grandparents.remove(signal.target)
+                    button_push_periods[signal.target] = button_push_count
                     break
-                
-                q.extend(self.propagate(message))
 
-        return math.lcm(*periods.values())
+                signal_queue.extend(self.propagate(signal))
+
+        return math.lcm(*button_push_periods.values())
 
 
-def main():
-    digraph, flipflops, conjunctions = read_input("input.txt")
-    system = System(digraph, flipflops, conjunctions)
+def main() -> None:
+    digraph, not_gates, nand_gates, broadcaster_gate = read_input("input.txt")
+    system = Circuit(digraph, not_gates, nand_gates, broadcaster_gate)
 
     for _ in range(1_000):
         system.push_button()
