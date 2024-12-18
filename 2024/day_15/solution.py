2b3edc8289b05b08eddbcb05701103fa3e55b165
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2024-12-18 23:50:09 +0200

    2024 day_15 (python)

--- 2024/day_15/solution.py
+++ 2024/day_15/solution.py
@@ -29,63 +29,86 @@ def can_move(
     dx, dy = direction
     xx, yy = x + dx, y + dy
 
-    match grid[xx][yy]:
-        case "#":
+    match dx, grid[xx][yy]:
+        case _, "#":
             return False
-        case ".":
+        case _, ".":
             return True
-        case "O":
+        case _, "O":
             return can_move(grid, (xx, yy), direction)
+        case 0, "[" | "]":
+            return can_move(grid, (xx, yy), direction)
+        case _, "[":
+            return can_move(grid, (xx, yy), direction) and can_move(
+                grid, (xx, yy + 1), direction
+            )
+        case _, "]":
+            return can_move(grid, (xx, yy), direction) and can_move(
+                grid, (xx, yy - 1), direction
+            )
         case _:
             raise ValueError(f"Invalid symbol {grid[xx][yy]}")
 
 
-def move(
+def unsafe_single_step_move(
     grid: list[list[str]], position: tuple[int, int], direction: tuple[int, int]
 ) -> None:
     x, y = position
     dx, dy = direction
     xx, yy = x + dx, y + dy
 
-    match grid[xx][yy]:
-        case "#":
+    match dx, grid[xx][yy]:
+        case _, "#":
             raise ValueError("Invalid Move")
-        case ".":
-            grid[x][y], grid[xx][yy] = grid[xx][yy], grid[x][y]
-        case "O":
-            move(grid, (xx, yy), direction)
-            grid[x][y], grid[xx][yy] = grid[xx][yy], grid[x][y]
+        case _, ".":
+            pass
+        case _, "O":
+            unsafe_single_step_move(grid, (xx, yy), direction)
+        case 0, "[" | "]":
+            unsafe_single_step_move(grid, (xx, yy), direction)
+        case _, "[":
+            unsafe_single_step_move(grid, (xx, yy + 1), direction)
+            unsafe_single_step_move(grid, (xx, yy), direction)
+        case _, "]":
+            unsafe_single_step_move(grid, (xx, yy), direction)
+            unsafe_single_step_move(grid, (xx, yy - 1), direction)
         case _:
             raise ValueError(f"Invalid symbol {grid[xx][yy]}")
 
+    grid[x][y], grid[xx][yy] = grid[xx][yy], grid[x][y]
 
-def print_grid(grid: list[list[str]], position: tuple[int, int]) -> None:
-    print(
-        "\n".join(
-            "".join(
-                grid[i][j] if (i, j) != position else "@" for j in range(len(grid[0]))
-            )
-            for i in range(len(grid))
-        )
+
+def safe_multi_step_move(
+    grid: list[list[str]], position: tuple[int, int], directions: list[tuple[int, int]]
+) -> None:
+    p = position
+    for direction in directions:
+        if can_move(grid, p, direction):
+            unsafe_single_step_move(grid, p, direction)
+            p = (p[0] + direction[0], p[1] + direction[1])
+
+
+def gps_sum(grid: list[list[str]]) -> int:
+    return sum(
+        100 * i + j
+        for i in range(len(grid))
+        for j in range(len(grid[0]))
+        if grid[i][j] in {"O", "["}
     )
 
 
 def main():
     file_path = sys.argv[1]
+
     grid, position, directions = read_input(file_path, expanded=False)
-    for direction in directions:
-        if can_move(grid, position, direction):
-            move(grid, position, direction)
-            position = position[0] + direction[0], position[1] + direction[1]
-    print_grid(grid, position)
-    print()
-    part_1 = sum(
-        100 * i + j
-        for i in range(len(grid))
-        for j in range(len(grid[0]))
-        if grid[i][j] == "O"
-    )
-    print(part_1)
+    safe_multi_step_move(grid, position, directions)
+    part_1 = gps_sum(grid)
+
+    grid, position, directions = read_input(file_path, expanded=True)
+    safe_multi_step_move(grid, position, directions)
+    part_2 = gps_sum(grid)
+
+    print(f"{part_1 = } {part_2 = }")
 
 
 if __name__ == "__main__":
