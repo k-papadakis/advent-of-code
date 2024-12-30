0fd44014d35abf7bc92199dc7e17861c96d711aa
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2024-12-30 18:49:05 +0200

    2024 day_21 part_2 (python)

--- 2024/day_21/solution.py
+++ 2024/day_21/solution.py
@@ -1,12 +1,6 @@
-from functools import cache, reduce
-from itertools import pairwise, starmap
 import sys
-
-
-def read_input(file_path: str) -> list[str]:
-    with open(file_path) as f:
-        return f.read().splitlines()
-
+from functools import cache
+from itertools import pairwise
 
 NUMPAD = [
     ["7", "8", "9"],
@@ -26,18 +20,16 @@ DIRPAD_POS = {
     DIRPAD[i][j]: (i, j) for i in range(len(DIRPAD)) for j in range(len(DIRPAD[0]))
 }
 
-ATOD = {
-    ">": (0, 1),
-    "^": (-1, 0),
-    "<": (0, -1),
-    "v": (1, 0),
-}
-DTOA = {v: k for k, v in ATOD.items()}
+DIRECTIONS = [(">", (0, 1)), ("^", (-1, 0)), ("<", (0, -1)), ("v", (1, 0))]
+
+
+def read_input(file_path: str) -> list[str]:
+    with open(file_path) as f:
+        return f.read().splitlines()
 
 
 @cache
 def shortest_paths(source_str: str, target_str: str) -> list[str]:
-    # TODO: make this function recursive to avoid recomputation
     if source_str in DIRPAD_POS and target_str in DIRPAD_POS:
         pad = DIRPAD
         source = DIRPAD_POS[source_str]
@@ -47,7 +39,7 @@ def shortest_paths(source_str: str, target_str: str) -> list[str]:
         source = NUMPAD_POS[source_str]
         target = NUMPAD_POS[target_str]
     else:
-        raise ValueError("source and target are not in the same pad")
+        raise ValueError("`source` and `target` are not in the same pad.")
 
     res: list[str] = []
     l1 = abs(target[0] - source[0]) + abs(target[1] - source[1])
@@ -63,32 +55,38 @@ def shortest_paths(source_str: str, target_str: str) -> list[str]:
             continue
         if pos == target:
             res.append(path)
-        for c, d in ATOD.items():
+        for c, d in DIRECTIONS:
             stack.append(((pos[0] + d[0], pos[1] + d[1]), path + c))
     return res
 
 
-def join_two(paths: list[str], other_paths: list[str]) -> list[str]:
-    return [path + "A" + other_path for path in paths for other_path in other_paths]
+@cache
+def shortest_path_length(source: str, target: str, num_robot_dirpads: int) -> int:
+    paths = shortest_paths(source, target)
+    if num_robot_dirpads == 0:
+        return 1 + min(map(len, paths))  # 1 is for the A that needs to be pressed
+    return min(
+        sum(
+            shortest_path_length(a, b, num_robot_dirpads - 1)
+            for a, b in pairwise("A" + path + "A")
+        )
+        for path in paths
+    )
 
 
-def next_seq(seq: str) -> list[str]:
-    return reduce(join_two, starmap(shortest_paths, pairwise("A" + seq + "A")))
+def code_complexity(code: str, num_robot_dirpads: int) -> int:
+    path_len = sum(
+        shortest_path_length(a, b, num_robot_dirpads) for a, b in pairwise("A" + code)
+    )
+    return path_len * int(code[:-1])
 
 
 def main():
     file_path = sys.argv[1]
     codes = read_input(file_path)
-
-    part_1 = 0
-    for code in codes:
-        paths_1 = next_seq(code)
-        paths_2 = [seq for path in paths_1 for seq in next_seq(path)]
-        paths_3 = [seq for path in paths_2 for seq in next_seq(path)]
-        min_len = min(map(len, paths_3))
-        code_int = int(code[:-1])
-        part_1 += min_len * code_int
-    print(part_1)
+    part_1 = sum(code_complexity(code, 2) for code in codes)
+    part_2 = sum(code_complexity(code, 25) for code in codes)
+    print(f"{part_1 = } {part_2 = }")
 
 
 if __name__ == "__main__":
