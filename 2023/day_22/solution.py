78413cd0560611f0ec6f6d7e466d6daa49f8ec57
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2025-01-05 20:10:15 +0200

    2023 day_22 part_2 (python)

--- 2023/day_22/solution.py
+++ 2023/day_22/solution.py
@@ -1,6 +1,6 @@
 import sys
 from dataclasses import dataclass
-from collections import defaultdict
+from collections import defaultdict, deque, Counter
 
 
 @dataclass(slots=True, frozen=True)
@@ -24,14 +24,11 @@ def read_input(file_path: str) -> list[Brick]:
     return bricks
 
 
-def build_brick_graph(
-    bricks: list[Brick],
-) -> tuple[defaultdict[int, set[int]], defaultdict[int, set[int]]]:
+def build_brick_graph(bricks: list[Brick]) -> defaultdict[int, set[int]]:
     x_max = max(brick.x_max for brick in bricks)
     y_max = max(brick.y_max for brick in bricks)
     heights = [[0 for _ in range(1 + y_max)] for _ in range(1 + x_max)]
     brick_ids = [[-1 for _ in range(1 + y_max)] for _ in range(1 + x_max)]
-    supporters: defaultdict[int, set[int]] = defaultdict(set)
     supportees: defaultdict[int, set[int]] = defaultdict(set)
     for brick_id, brick in sorted(enumerate(bricks), key=lambda t: t[1].z_min):
         rx = range(brick.x_min, brick.x_max + 1)
@@ -42,23 +39,41 @@ def build_brick_graph(
                 if heights[x][y] == height:
                     supporter_id = brick_ids[x][y]
                     if supporter_id != -1:
-                        supporters[brick_id].add(supporter_id)
                         supportees[supporter_id].add(brick_id)
                 heights[x][y] = height + brick.z_max - brick.z_min + 1
                 brick_ids[x][y] = brick_id
-    return supporters, supportees
+    return supportees
+
+
+def count_falls(brick_id: int, supportees: dict[int, set[int]]) -> int:
+    num_fallen = 0
+    in_degree = Counter(
+        supportee for supporter in supportees for supportee in supportees[supporter]
+    )
+    q = deque([brick_id])
+    while q:
+        u = q.popleft()
+        for v in supportees[u]:
+            in_degree[v] -= 1
+            if in_degree[v] == 0:
+                num_fallen += 1
+                q.append(v)
+    return num_fallen
 
 
 def main():
     file_path = sys.argv[1]
     bricks = read_input(file_path)
-    supporters, supportees = build_brick_graph(bricks)
-
-    part_1 = sum(
-        all(len(supporters[supportee]) > 1 for supportee in supportees[brick_id])
-        for brick_id in range(len(bricks))
-    )
-    print(part_1)
+    supportees = build_brick_graph(bricks)
+    part_1 = 0
+    part_2 = 0
+    for brick_id in range(len(bricks)):
+        num_fallen = count_falls(brick_id, supportees)
+        if num_fallen == 0:
+            part_1 += 1
+        else:
+            part_2 += num_fallen
+    print(f"{part_1 = } {part_2 = }")
 
 
 if __name__ == "__main__":
