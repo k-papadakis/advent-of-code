8f86a864927d20296c22a6e128d45664ba039a17
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2025-03-07 01:34:09 +0200

    2015 day_05 part_2 (rust)

--- 2015/day_05/src/main.rs
+++ 2015/day_05/src/main.rs
@@ -13,6 +13,20 @@ fn contains_prohibited(s: &str) -> bool {
         .any(|(a, b)| [('a', 'b'), ('c', 'd'), ('p', 'q'), ('x', 'y')].contains(&(a, b)))
 }
 
+fn contains_pair_twice_without_overlap(s: &str) -> bool {
+    let mut windows = s.as_bytes().windows(2);
+    while let Some(u) = windows.next() {
+        if windows.clone().skip(1).any(|v| u == v) {
+            return true;
+        }
+    }
+    false
+}
+
+fn contains_repeated_with_one_intermediary(s: &str) -> bool {
+    s.as_bytes().windows(3).any(|window| window[0] == window[2])
+}
+
 fn main() {
     let file_path = env::args().nth(1).unwrap();
     let s = fs::read_to_string(file_path).unwrap();
@@ -26,5 +40,13 @@ fn main() {
         })
         .count();
 
-    println!("part_1 = {part_1}");
+    let part_2 = s
+        .lines()
+        .filter(|&line| {
+            contains_pair_twice_without_overlap(line)
+                && contains_repeated_with_one_intermediary(line)
+        })
+        .count();
+
+    println!("part_1 = {part_1}, part_2 = {part_2}");
 }
