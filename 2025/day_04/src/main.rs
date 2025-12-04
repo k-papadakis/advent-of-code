351a473ee3f3b8676152e89eb9e23fe176d5f611
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2025-12-04 16:18:10 +0200

    refactor(25-4,rust): clearer functions

--- 2025/day_04/src/main.rs
+++ 2025/day_04/src/main.rs
@@ -52,31 +52,23 @@ fn is_accessible(grid: &[Vec<bool>], (i, j): (usize, usize)) -> bool {
         < 4
 }
 
-fn find_accessible(grid: &[Vec<bool>]) -> impl Iterator<Item = (usize, usize)> {
+fn iter_papers(grid: &[Vec<bool>]) -> impl Iterator<Item = (usize, usize)> {
     let (m, n) = (grid.len(), grid[0].len());
-    let positions = (0..m)
+
+    (0..m)
         .flat_map(move |i| (0..n).map(move |j| (i, j)))
-        .filter(|&(i, j)| grid[i][j]);
-    positions.filter(|&pos| is_accessible(grid, pos))
+        .filter(|&(i, j)| grid[i][j])
 }
 
-#[allow(dead_code)]
-fn grid_to_str(grid: &[Vec<bool>]) -> String {
-    let mut res = String::with_capacity((grid.len()) * (grid[0].len() + 1));
-    for row in grid {
-        for &b in row {
-            let c = if b { '@' } else { '.' };
-            res.push(c);
-        }
-        res.push('\n');
-    }
-    res
+fn iter_accessible_papers(grid: &[Vec<bool>]) -> impl Iterator<Item = (usize, usize)> {
+    iter_papers(grid).filter(|&pos| is_accessible(grid, pos))
 }
 
-fn iter_accessible_with_removal(grid: &[Vec<bool>]) -> impl Iterator<Item = Vec<(usize, usize)>> {
+fn iter_accessible_paper_removals(grid: &[Vec<bool>]) -> impl Iterator<Item = Vec<(usize, usize)>> {
     let mut grid = grid.to_vec();
+
     iter::from_fn(move || {
-        let accessibles: Vec<_> = find_accessible(&grid).collect();
+        let accessibles: Vec<_> = iter_accessible_papers(&grid).collect();
 
         if accessibles.is_empty() {
             return None;
@@ -90,24 +82,13 @@ fn iter_accessible_with_removal(grid: &[Vec<bool>]) -> impl Iterator<Item = Vec<
     })
 }
 
-#[allow(dead_code)]
-fn accessible_grid_to_str(grid: &[Vec<bool>]) -> String {
-    let n = grid[0].len();
-    let mut grid_str = grid_to_str(grid).into_bytes();
-    let accessible_positions = find_accessible(grid);
-    for (i, j) in accessible_positions {
-        grid_str[i * (n + 1) + j] = b'x';
-    }
-    String::from_utf8(grid_str).unwrap()
-}
-
 fn main() -> Result<(), Box<dyn Error>> {
     let file_path = env::args().nth(1).ok_or("file path not provided")?;
     let contents = fs::read_to_string(file_path)?;
     let grid = parse(contents);
 
-    let part_1 = find_accessible(&grid).count();
-    let part_2: usize = iter_accessible_with_removal(&grid).map(|v| v.len()).sum();
+    let part_1 = iter_accessible_papers(&grid).count();
+    let part_2: usize = iter_accessible_paper_removals(&grid).map(|v| v.len()).sum();
 
     println!("Part 1: {part_1}");
     println!("Part 2: {part_2}");
