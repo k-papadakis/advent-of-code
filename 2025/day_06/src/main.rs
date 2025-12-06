71b80bb510efbb8d457199b27f82600699aafb05
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2025-12-06 19:42:32 +0200

    feat(25-6,rust): solved

--- 2025/day_06/src/main.rs
+++ 2025/day_06/src/main.rs
@@ -7,7 +7,7 @@ enum Op {
     Mul,
 }
 
-fn parse(contents: impl AsRef<str>) -> (Vec<Vec<i64>>, Vec<Op>) {
+fn parse(contents: impl AsRef<str>) -> (Vec<Vec<u64>>, Vec<Vec<u64>>, Vec<Op>) {
     let lines: Vec<_> = contents.as_ref().lines().collect();
 
     let (ops_line, nums_lines) = lines.split_last().unwrap();
@@ -21,39 +21,61 @@ fn parse(contents: impl AsRef<str>) -> (Vec<Vec<i64>>, Vec<Op>) {
         })
         .collect();
 
-    let mut nums_iters: Vec<_> = nums_lines.iter().map(|s| s.split_whitespace()).collect();
-    let nums = (0..ops.len())
+    // Create an iterator of words on each row and advance them in lockstep.
+    let mut nums1_iters: Vec<_> = nums_lines.iter().map(|s| s.split_whitespace()).collect();
+    let nums1 = (0..ops.len())
         .map(|_| {
-            nums_iters
+            nums1_iters
                 .iter_mut()
                 .map(|it| it.next().unwrap().parse().unwrap())
                 .collect()
         })
         .collect();
 
-    (nums, ops)
+    // Create an iterator of characters on each row and advance them in lockstep.
+    // Each advancement results into Some(number) if the advancement returned any digits, otherwise None
+    // Then we split on None like so:
+    // Some(1), Some(23), None, Some(456), None, Some(7), Some(8)
+    // -> [[1, 23], [456], [7, 8]]
+    let mut digit_iters: Vec<_> = nums_lines.iter().map(|s| s.chars()).collect();
+    let nums2_opts: Vec<_> = (0..ops_line.len())
+        .map(|_| {
+            digit_iters
+                .iter_mut()
+                .map(|it| it.next().unwrap())
+                .filter_map(|c| c.to_digit(10).map(|x| x as u64))
+                .reduce(|x, y| 10 * x + y)
+        })
+        .collect();
+    let nums2 = nums2_opts
+        .split(|x| x.is_none())
+        .map(|x| x.iter().flatten().copied().collect())
+        .collect();
+
+    (nums1, nums2, ops)
+}
+
+fn solve(xs: &[u64], op: Op) -> u64 {
+    match op {
+        Op::Add => xs.iter().sum(),
+        Op::Mul => xs.iter().product(),
+    }
+}
+
+fn solve_sum(nums: &[Vec<u64>], ops: &[Op]) -> u64 {
+    nums.iter().zip(ops).map(|(xs, &op)| solve(xs, op)).sum()
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let file_path = env::args().nth(1).ok_or("file path not provided")?;
     let contents = fs::read_to_string(file_path)?;
-    let (nums, ops) = parse(contents);
+    let (nums1, nums2, ops) = parse(contents);
 
-    let part_1: i64 = nums
-        .iter()
-        .zip(ops)
-        .map(|(xs, op)| {
-            xs.iter()
-                .copied()
-                .reduce(|x, y| match op {
-                    Op::Mul => x * y,
-                    Op::Add => x + y,
-                })
-                .unwrap()
-        })
-        .sum();
+    let part_1 = solve_sum(&nums1, &ops);
+    let part_2 = solve_sum(&nums2, &ops);
 
     println!("Part 1: {part_1}");
+    println!("Part 2: {part_2}");
 
     Ok(())
 }
