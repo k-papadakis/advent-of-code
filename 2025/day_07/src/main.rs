2bcdf0c448c256a667a7731d23d0c59ac230730c
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2025-12-07 13:13:16 +0200

    refactor(25-7,rust): custom cache

--- 2025/day_07/src/main.rs
+++ 2025/day_07/src/main.rs
@@ -1,10 +1,9 @@
-use cached::proc_macro::cached;
 use std::{env, error::Error, fs};
 
-type Grid = Vec<Vec<bool>>;
+type Grid<T> = Vec<Vec<T>>;
 type Pos = (usize, usize);
 
-fn parse(contents: impl AsRef<str>) -> Result<(Grid, Pos), String> {
+fn parse(contents: impl AsRef<str>) -> Result<(Grid<bool>, Pos), String> {
     let mut start = None;
 
     let grid = contents
@@ -30,7 +29,7 @@ fn parse(contents: impl AsRef<str>) -> Result<(Grid, Pos), String> {
     Ok((grid?, start.ok_or("start not found")?))
 }
 
-fn count_splits(grid: &Grid, start: Pos) -> usize {
+fn count_splits(grid: &Grid<bool>, start: Pos) -> usize {
     let n = grid[0].len();
 
     let mut beams = vec![false; n];
@@ -56,27 +55,37 @@ fn count_splits(grid: &Grid, start: Pos) -> usize {
     res
 }
 
-#[cached(key = "Pos", convert = r#"{ start }"#)]
-fn count_timelines(grid: &Grid, start: Pos) -> usize {
-    if start.0 == grid.len() - 1 {
+fn count_timelines_memo(grid: &Grid<bool>, (i, j): Pos, cache: &mut Grid<Option<usize>>) -> usize {
+    if let Some(cache_hit) = cache[i][j] {
+        return cache_hit;
+    }
+
+    let (m, n) = (grid.len(), grid[0].len());
+
+    if i == m - 1 {
         return 1;
     }
 
-    if grid[start.0][start.1] {
-        let left = if start.1 > 0 {
-            count_timelines(grid, (start.0 + 1, start.1 - 1))
-        } else {
-            0
+    let res = if grid[i][j] {
+        let mut acc = 0;
+        if j > 0 {
+            acc += count_timelines_memo(grid, (i + 1, j - 1), cache)
+        }
+        if j < n - 1 {
+            acc += count_timelines_memo(grid, (i + 1, j + 1), cache)
         };
-        let right = if start.1 + 1 < grid.len() {
-            count_timelines(grid, (start.0 + 1, start.1 + 1))
-        } else {
-            0
-        };
-        left + right
+        acc
     } else {
-        count_timelines(grid, (start.0 + 1, start.1))
-    }
+        count_timelines_memo(grid, (i + 1, j), cache)
+    };
+
+    cache[i][j] = Some(res);
+    res
+}
+
+fn count_timelines(grid: &Grid<bool>, start: Pos) -> usize {
+    let mut cache = vec![vec![None; grid[0].len()]; grid.len()];
+    count_timelines_memo(grid, start, &mut cache)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
